cfd645ac1e1fba834591e4776710295ddc7140e7
Remi Zlatinis <remizlatinis@gmail.com>
2023-08-18 10:57:56 +0300

    Implement uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-# Check if is installed
-if [ ! -d "/etc/service-status-indicator-api/" ]; then
-    echo "Service Status Indicator API is not installed."
-    exit 0 
-fi
-
-
 # Check for root privileges 
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root" 
     exit 1
 fi
 
+# TODO: Add confirmation.
 
-# Disable systemd service
-systemctl disable --now service-status-indicator-api.service
-
-
-# Remove files
-rm -rf /etc/service-status-indicator-api/
+systemctl disable --now service-status-indicator-api.service &> /dev/null
+systemctl disable --now service-status-indicator-scheduler.service &> /dev/null
+rm /etc/systemd/system/service-status-indicator-* &> /dev/null
+rm /usr/bin/local/ssi &> /dev/null
+rm -rf /etc/service-status-indicator/ &> /dev/null
+echo "✅ Uninstall complete. Bye bye 👋"
